6cacbf39ca12ffa6f2a101b3a337d9d27827d690
vitoria_lnogueira <vitoria_lnogueira@unifesspa.edu.br>
2025-09-23 14:47:10 -0300

    add foreing key

--- api-fastify-knexjs/src/database/migrations/20250920170040_create-modules.ts
+++ api-fastify-knexjs/src/database/migrations/20250920170040_create-modules.ts
@@ -3,6 +3,7 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable("modules", (table) => {
     table.increments("id").primary();
+    table.integer("course_id").notNullable().references("id").inTable("courses")
     table.string("title");
     table.string("description");
   });
